e01819effdce07dd1da2b1c3861e32fe0b5ac1ab
AaronElam <speztra97@gmail.com>
2021-09-21 17:58:05 -0500

    Filter working

--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 const placeholders = [
   "Cabin",
   "House",
@@ -8,7 +10,30 @@ const placeholders = [
   "San Francisco, CA",
 ];
 
-export default function Filter() {
+export default function Filter(props) {
+  const [properties, setProperties] = useState(props.properties);
+  const [search, setSearch] = useState("");
+
+  const handleFilterChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    if (search.length > 0) {
+      setProperties(
+        properties.filter((property) => {
+          return property.description.match(search);
+        })
+      );
+    }
+    if (search.length === 0) {
+      setProperties(props.properties);
+    }
+    console.log(search);
+    console.log(properties);
+  }, [search]);
+
   // let handleInputChange = (e) => {
   //   let { name, value } = e.target;
   //   this.setState({
@@ -26,26 +51,26 @@ export default function Filter() {
     <form class="content-center w-full max-w-lg">
       <div class="flex items-center border-b border-teal-500 py-2">
         <input
-          name="filterText"
-          // value={this.state.inputs.filterText}
-          // onChange={handleInputChange}
+          name="filter"
+          value={search}
+          onChange={handleFilterChange}
           class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
           type="text"
           placeholder={
             placeholders[Math.floor(Math.random() * placeholders.length)]
           }
-          aria-label="filterText"
+          aria-label="filter"
         />
         <button
           class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 py-1 px-2 rounded"
-          type="button"
+          type="submit"
         >
           Filter
         </button>
         <button
           class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
           type="button"
-          // onclick={resetInputFields}
+          // onClick={() => setSearch("")}
         >
           Clear
         </button>
--- frontend/src/components/FilterablePropertyGrid.js
+++ frontend/src/components/FilterablePropertyGrid.js
@@ -4,13 +4,14 @@ import PropertyCard from "./PropertyCard";
 import Filter from "./Filter";
 
 const FilterablePropertyGrid = (props) => {
+  let properties = props.properties;
   return (
     <div class="container my-12 mx-auto px-4 md:px-12">
       <div class="flex flex-wrap justify-center">
-        <Filter />
+        <Filter properties={properties} />
       </div>
       <div class="flex flex-wrap -mx-1 lg:-mx-4">
-        {props.properties.map((property) => (
+        {properties.map((property) => (
           <div
             key={property.id}
             class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"
