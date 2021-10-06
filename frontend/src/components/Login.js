7835b706b263ac305c720d95c252bd80ffa66b2c
AaronElam <speztra97@gmail.com>
2021-10-05 22:06:04 -0500

    Syntax and mobile responsiveness

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -11,12 +11,12 @@ const Login = () => {
   };
   return (
     <div>
-      <h1 class="flex justify-center font-bold text-gray-800 text-2xl py-4">
+      <h1 className="flex justify-center font-bold text-gray-800 text-2xl py-4">
         Login
       </h1>
-      <div class="flex justify-center h-screen">
+      <div className="flex justify-center h-screen">
         <form
-          class="w-full max-w-lg"
+          className="w-full max-w-lg"
           onSubmit={(e) => {
             e.preventDefault();
             axios
@@ -32,14 +32,14 @@ const Login = () => {
           }}
         >
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="email"
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="email"
           >
             Email
           </label>
           <input
             id="email"
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-1 leading-tight focus:outline-none focus:bg-white"
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-1 leading-tight focus:outline-none focus:bg-white"
             type="text"
             name="email"
             placeholder="Email"
@@ -47,13 +47,13 @@ const Login = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="password"
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="password"
           >
             Password
           </label>
           <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="password"
             type="password"
             name="password"
@@ -63,7 +63,7 @@ const Login = () => {
           />
           <button
             disabled={!isEnabled}
-            class="w-full bg-yellow-500 rounded mt-6 px-24 py-4 text-lg font-semibold text-white mr-1 focus:ring-4 focus:ring-yellow-300 focus:ring-inset"
+            className="w-full bg-yellow-500 rounded mt-6 px-24 py-4 text-lg font-semibold text-white mr-1 focus:ring-4 focus:ring-yellow-300 focus:ring-inset"
             type="submit"
             ///TODO: fix if required forms are not filled out
           >
--- frontend/src/components/SkeletonCard.js
+++
@@ -1,3 +0,0 @@
-export default function SkeletonCard() {
-  //todo
-}
